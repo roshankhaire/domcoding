6791de2642d8a8ae668cae33a219ad8727ea4766
roshankhaire <roshankhaire1992@gmail.com>
2022-08-11 15:30:53 -0700

    first commit

---
+++ codingdom.js
@@ -0,0 +1,34 @@
+/*console.log('hellow');
+console.dir(document);
+console.log(document.domain);
+console.log(document.URL);
+console.log(document.title);
+document.title=123;
+console.log(document.doctype);
+console.log(document.head);
+//console.log(document.body);
+//console.log(document.all);
+//console.log(document.all[10]);
+//document.all[10].textContent='hellow';
+console.log(document.forms);
+console.log(document.links);
+console.log(document.forms[0]);*/
+//console.log(document.images);
+//console.log(document.getElementById('header-title'));
+var headerTitle=document.getElementById('header-title');
+//console.log(headerTitle);
+//headerTitle.textContent='hi';
+//headerTitle.innerHTML='good';
+headerTitle.style.borderBottom ="solid 3px #000";
+//var header=document.getElementById('main-header');
+/*header.style.borderBottom ="solid 3px #000";
+var items=document.getElementsByClassName('list-group-item');
+console.log(items);
+console.log(items[0]);
+items[1].textContent='hellow2'
+items[1].style.fontWeight='bold';
+items[1].style.backgroundColor='green';
+*/
+var res=document.querySelector('h2');
+console.log(res);
+res.style.color='green';
